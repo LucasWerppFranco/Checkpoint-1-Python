d8200a043df98c7c52efc5cb6e457f94aa17a58d
LucasWerppFranco <146235988+LucasWerppFranco@users.noreply.github.com>
2025-04-02 17:55:37 -0300

    Arquivo 'pcp_checkpoint1_ex03.py' criado e terceira atividade concluida

---
+++ pcp_checkpoint1_ex03.py
@@ -0,0 +1,7 @@
+anos = int(input("Digite o ano que você naceu: "))
+meses = int(input("Digite o meses que você naceu: "))
+dias = int(input("Digite o dias que você naceu: "))
+
+total_dias = (anos * 365) + (meses * 30) + dias
+
+print(f"Você viveu por {total_dias} dias. Continue assim!!! ;) ")
