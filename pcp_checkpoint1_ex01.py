ba2756dadaaeb2e79759c46a2d091f2ec517e8f1
LucasWerppFranco <146235988+LucasWerppFranco@users.noreply.github.com>
2025-04-03 13:06:10 -0300

    reorganizando a primeira atividade

--- pcp_checkpoint1_ex01.py
+++ pcp_checkpoint1_ex01.py
@@ -1,23 +1,35 @@
-reais = float(input("Insira um valor em reais: "))
+def conversao_de_moedas(reais):
+    escolha = input(
+        "Para qual moeda você gostaria de converter o seu valor em reais\n (1) Dólar Americano\n (2) Euro\n (3) Peso Argentino\n (4) Libra Esterlina\n (5) Iene\nInsira o numero da moeda escolhida:"
+    )
 
-escolha = input(
-    "Para qual moeda você gostaria de converter o seu valor em reais\n (1) Dólar Americano\n (2) Euro\n (3) Peso Argentino\n (4) Libra Esterlina\n (5) Iene\nInsira o numero da moeda escolhida:"
-)
+    if escolha == "1":
+        moeda = round(reais / 5.68, 2)
+        icone = "$"
+    elif escolha == "2":
+        moeda = round(reais / 6.7, 2)
+        icone = "€"
+    elif escolha == "3":
+        moeda = round(reais * 188, 2)
+        icone = "P$"
+    elif escolha == "4":
+        moeda = round(reais / 7.40, 2)
+        icone = "£"
+    elif escolha == "5":
+        moeda = round(reais * 26, 2)
+        icone = "¥"
+    else:
+        print("Por favor, adicione um numero valido")
+        return None, None  # Retorna None se a escolha for inválida
 
-if escolha == "1":
-    dolares = round(reais / 5.68, 2)
-    print(f"Seu saldo é de ${dolares} Dólares Americanos.")
-elif escolha == "2":
-    euros = round(reais / 6.7, 2)
-    print(f"Seu saldo é de €{euros} Euros.")
-elif escolha == "3":
-    pesos = round(reais * 188, 2)
-    print(f"Seu saldo é de ${pesos} Pesos Argentinos.")
-elif escolha == "4":
-    libras = round(reais / 7.40, 2)
-    print(f"Seu saldo é de £ {libras} Libras Esterlinas.")
-elif escolha == "5":
-    ienes = round(reais * 26, 2)
-    print(f"Seu saldo é de ¥ {ienes} Ienes.")
-else:
-    print("Por favor, adicione um numero valido")
+    return moeda, icone
+
+
+try:
+    reais = float(input("Insira um valor em Reais: "))
+    moeda, icone = conversao_de_moedas(reais)  # Passa reais como argumento
+    if moeda is not None and icone is not None:
+        print(f"Seu saldo é de {icone}{moeda}")
+except ValueError:
+    print("Por favor, Insira um valor valido")
+
