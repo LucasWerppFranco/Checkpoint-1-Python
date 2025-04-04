5d2f3fd15295aa95aa72b64ae74f5087e4e54f8b
LucasWerppFranco <146235988+LucasWerppFranco@users.noreply.github.com>
2025-04-04 09:00:30 -0300

    Sistema de conversão da atividade um concertado

--- pcp_checkpoint1_ex01.py
+++ pcp_checkpoint1_ex01.py
@@ -1,22 +1,22 @@
 def conversao_de_moedas(reais):
     escolha = input(
-        "Para qual moeda você gostaria de converter o seu valor em reais\n (1) Dólar Americano\n (2) Euro\n (3) Peso Argentino\n (4) Libra Esterlina\n (5) Iene\nInsira o numero da moeda escolhida:"
+        "Para qual moeda você gostaria de converter o seu valor em reais\n (1) Dólar Americano\n (2) Euro\n (3) Peso Argentino\n (4) Libra Esterlina\n (5) Iene\nInsira o numero da moeda escolhida: "
     )
 
     if escolha == "1":
-        moeda = round(reais / 5.68, 2)
+        moeda = round(reais * 0.18)
         icone = "$"
     elif escolha == "2":
-        moeda = round(reais / 6.7, 2)
+        moeda = round(reais * 0.16)
         icone = "€"
     elif escolha == "3":
-        moeda = round(reais * 188, 2)
+        moeda = round(reais * 190.83)
         icone = "P$"
     elif escolha == "4":
-        moeda = round(reais / 7.40, 2)
+        moeda = round(reais * 0.14)
         icone = "£"
     elif escolha == "5":
-        moeda = round(reais * 26, 2)
+        moeda = round(reais * 25.79)
         icone = "¥"
     else:
         print("Por favor, adicione um numero valido")
